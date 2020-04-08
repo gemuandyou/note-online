52b6a1efa7c60fe381a52910f9ccf44a44f62b2e
Gemu <gemuandyou@163.com>
2020-04-08 17:09:50 +0800

    添加看板娘

---
+++ src/assets/plugin/live2d/live2d.init.js
@@ -0,0 +1,9 @@
+L2Dwidget.init({"display": {
+        "superSample": 2,
+        "width": 200,
+        "height": 400,
+        "position": "right",
+        "hOffset": 0,
+        "vOffset": 0
+    }
+});
