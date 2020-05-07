0ae80aadb32ffcff9f20bc3b1f289fb960b39419
Gemu <gemuandyou@163.com>
2020-05-07 16:53:52 +0800

    添加看板娘的拖动

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,45 @@ export class AppComponent implements AfterViewInit {
             this.navigation.nativeElement.remove();
             this.searchDom.nativeElement.style.width = '38%';
         }
+
+        // 修改看板娘样式并添加拖动
+        const live2dHandle = setInterval(() => {
+            console.count('loading live2d');
+            const live2D = document.getElementById('live2d-widget');
+            const live2DCanvas = document.getElementById('live2dcanvas');
+            if (live2D && live2DCanvas) {
+                clearInterval(live2dHandle);
+                console.log(live2D);
+                live2D.style.pointerEvents = 'auto';
+
+                live2D.onmousedown = function (event: MouseEvent) {
+                    event = event || <MouseEvent>window.event;
+                    const diffX = event.clientX - live2D.offsetLeft;
+                    const diffY = event.clientY - live2D.offsetTop;
+                    document.onmousemove = function (event: MouseEvent) {
+                        event = event || <MouseEvent>window.event;
+                        let moveX = event.clientX - diffX;
+                        let moveY = event.clientY - diffY;
+                        if (moveX < 0) {
+                            moveX = 0
+                        } else if (moveX > window.innerWidth - live2D.offsetWidth) {
+                            moveX = window.innerWidth - live2D.offsetWidth
+                        }
+                        if (moveY < 0) {
+                            moveY = 0
+                        } else if (moveY > window.innerHeight - live2D.offsetHeight) {
+                            moveY = window.innerHeight - live2D.offsetHeight
+                        }
+                        live2D.style.left = moveX + 'px';
+                        live2D.style.top = moveY + 'px'
+                    }
+                    document.onmouseup = function (event) {
+                        this.onmousemove = null;
+                        this.onmouseup = null;
+                    }
+                }
+            }
+        }, 500);
     }
 
     toSearch(searchKey: string): void {
