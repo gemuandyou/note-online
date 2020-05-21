4cc8737023b30f70412d61a5dab3f15f8a583ec2
Gemu <gemuandyou@163.com>
2020-05-21 17:32:31 +0800

    修改live2d看板图片

--- src/assets/plugin/live2d/live2d.init.js
+++ src/assets/plugin/live2d/live2d.init.js
@@ -1,9 +1,14 @@
-L2Dwidget.init({"display": {
+L2Dwidget.init({
+    "display": {
         "superSample": 2,
         "width": 200,
         "height": 400,
         "position": "right",
         "hOffset": 0,
         "vOffset": 0
+    },
+    "model": { 
+        "jsonPath": "assets/plugin/live2d/umaru/model.json", 
+        "scale": 1 
     }
 });
---
+++ src/assets/plugin/live2d/model-url.md
@@ -0,0 +1,2 @@
+# live2d模型下载
+[地址](https://mx.paul.ren/)
